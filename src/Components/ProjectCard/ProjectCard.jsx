0ce2be0bdb2ed54a1d149e3910bd8edc69b10d35
Ehtesham Zahid <shamimalick321@gmail.com>
2023-09-28 23:54:44 +0500

    Website Design Updated

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -1,31 +1,15 @@
-// import './ProjectCard.css';
+import './ProjectCard.css';
 
 import { Link } from 'react-router-dom';
 // import Imagee from '../../utils/ProjectsImages/Portfolio.png';
 
 const ProjectCard = (props) => {
   return (
-    // <Link
-    //   to={props.link}
-    //   target="_blank"
-    //   className="card flex flex-col justify-around items-center w-36 h-56 sm:w-48 sm:h-60"
-    // >
-    //   <img
-    //     src={props.image}
-    //     className="z-10 bg-white  w-32 sm:w-40 rounded-xl"
-    //   />
-    //   <div className="text-red-100 text-center z-10 text-md text-semibold mt-3 hover:underline hover:text-red-500">
-    //     {props.title}
-    //   </div>
-    // </Link>
-    <div className="card card-compact w-64 sm:w-72 bg-base-100 shadow-xl">
-      <figure>
-        <img src={props.image} className="w-48" alt="Shoes" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title text-white">{props.title}</h2>
-
-        <div className="card-actions justify-end">
+    <div className="card cursor-pointer">
+      <div className="card2 flex flex-col justify-center rounded-lg">
+        <img src={props.image} className="h-40 m-2" />
+        <p className="card-title text-white m-2">{props.title}</p>
+        <div className="card-actions justify-end m-2">
           <Link
             to={props.link}
             target="_blank"
